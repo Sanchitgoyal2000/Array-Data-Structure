d0a5d86880bac38194741e8ad845a48c976ad504
Sanchitgoyal2000 <38776804+Sanchitgoyal2000@users.noreply.github.com>
2019-10-30 13:30:02 +0530

    Add files via upload

---
+++ Find Missing nd repeating element.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t-->0)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	        a[i]=a[i]-1;
+	    }
+
+	    for(int i=0;i<n;i++) //calculating frequency
+	    {
+	        a[a[i]%n]=a[a[i]%n]+n;   // important step must take modulus
+	    }
+	    int h=0,rep,mis;
+	   //
+	    for(int i=0;i<n;i++)
+	    {
+	        if(a[i]<n)
+	        {
+	            mis=i+1;
+	        }
+	        else if(h==0&&(a[i]/n)>=2)
+	        {
+	            rep=i+1;
+	            h=1;
+	        }
+	    }
+	    cout<<rep<<" "<<mis<<"\n";
+	}
+	return 0;
+}
