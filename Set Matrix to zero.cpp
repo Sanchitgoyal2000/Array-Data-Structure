c86c770ad81e2dbb8d5955bf922fde22a6e479aa
Sanchitgoyal2000 <38776804+Sanchitgoyal2000@users.noreply.github.com>
2019-11-02 23:46:06 +0530

    Update Set Matrix to zero.cpp

--- Set Matrix to zero.cpp
+++ Set Matrix to zero.cpp
@@ -1,3 +1,95 @@
+#include <iostream>
+using namespace std;
+
+int main() {         // Space Complexity is O(1)
+	//code
+	int t;
+	cin>>t;
+	while(t-->0)
+	{
+	    int n1,n2;
+	    cin>>n1>>n2;
+	    
+	    int A[n1][n2];         
+	  
+	    for(int i=0;i<n1;i++)
+         {
+           for(int j=0;j<n2;j++)
+            {
+              cin>>A[i][j];
+            }
+         }
+         
+    int i,j,rf=1,cf=1;
+    
+    for(i=0;i<n1;i++)
+    {
+        if(A[i][0]==1)
+        {
+          rf=0;
+          break;
+        }
+    }
+    
+    for(j=0;j<n2;j++)
+    {
+        if(A[0][j]==1)
+        {
+          cf=0;
+          break;
+        }
+    }
+    
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+           if(A[i][j]==1)
+           {
+               A[i][0]=1;
+               A[0][j]=1;
+           }
+        }
+    }
+    
+    
+    for(i=1;i<n1;i++)
+    {
+        for(j=1;j<n2;j++)
+        {
+            if(A[i][0]==1||A[0][j]==1)
+            {
+                A[i][j]=1;
+            }
+        }
+    }
+    
+    if(rf==0)
+    {
+        for(i=0;i<n2;i++)
+          A[0][i]=1;
+    }
+    
+    if(cf==0)
+    {
+        for(j=0;j<n1;j++)
+          A[j][0]=1;
+    }
+    
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+            cout<<A[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+	}
+	return 0;
+}
+
+
+------------------------------------------------------------------------------------------------------------------------------
 void setZeroes(int** A, int n1, int n2)
 {
     int row[n1],col[n2];    // Space Compleity is O(m+n) time O(m*n)
